4b8386ff8aa89a98c2c6684f016f9f732f664c33
sweety <122354455+raghvikumari@users.noreply.github.com>
2023-05-03 00:27:41 +0530

    changed design

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".multiple-text",{
     strings: ["Frontend Develper","Competitive Programmer","Blogger"],
-    typeSpeed: 100,
-    backSpeed: 100,
+    typeSpeed: 180,
+    backSpeed: 50,
     backDelay: 1000,
     loop: true
 })
